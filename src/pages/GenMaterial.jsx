5d1ff03acb62c72dd9660a3090b492f3a80de633
haitao <e1070551@u.nus.edu>
2024-04-18 19:23:19 +0800

    improving quiz UI

--- src/pages/GenMaterial.jsx
+++ src/pages/GenMaterial.jsx
@@ -3,9 +3,43 @@ import { useNavigate } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import {Button, Text, Stack, Box} from '@chakra-ui/react';
 import Markdown from 'react-markdown'
+import MCQQuestion from "./MCQQuestion.jsx";
+const DemoQuizContent = `
+<question>What is the main purpose of an autoencoder?</question>
+<option>A. To generate new data similar to the training data.</option> <option>B. To classify data into different categories.</option> <option>C. To predict future values based on past data.</option> <option>D. To translate languages.</option> <explanation>Autoencoders are a type of neural network that learns to compress and then reconstruct its input data. This compressed representation can then be used to generate new data that is similar to the training data.</explanation>
 
+<question>What are the two main components of an autoencoder?</question>
+<option>A. Encoder and decoder</option> <option>B. Input and output layer</option> <option>C. Convolutional and pooling layers</option> <option>D. Hidden and output layer</option> <explanation>The encoder compresses the input data into a lower-dimensional representation, while the decoder reconstructs the data from this compressed representation.</explanation>
+
+<question>What is the bottleneck layer in an autoencoder?</question>
+<option>A. The first layer of the encoder</option> <option>B. The last layer of the decoder</option> <option>C. The layer with the smallest number of neurons</option> <option>D. The layer with the largest number of neurons</option> <explanation>The bottleneck layer is the layer in the autoencoder that has the smallest number of neurons. This forces the autoencoder to learn a more efficient representation of the data.</explanation>
+
+<question>What are the different types of autoencoders?</question>
+<option>A. Variational autoencoders (VAEs) and denoising autoencoders</option> <option>B. Convolutional autoencoders (CAEs) and stacked autoencoders</option> <option>C. Sparse autoencoders and contractive autoencoders</option> <option>D. All of the above</option> <explanation>There are many different types of autoencoders, each with its own strengths and weaknesses. Some common types include VAEs, CAEs, stacked autoencoders, sparse autoencoders, and contractive autoencoders.</explanation>
+
+<question>What are some applications of autoencoders?</question>
+<option>A. Image denoising and compression</option> <option>B. Anomaly detection and outlier identification</option> <option>C. Feature extraction and dimensionality reduction</option> <option>D. All of the above</option> <explanation>Autoencoders have a wide range of applications, including image denoising and compression, anomaly detection and outlier identification, feature extraction and dimensionality reduction, and many others.</explanation>
+
+<question>What are the limitations of autoencoders?</question>
+<option>A. They can be computationally expensive to train.</option> <option>B. They can be sensitive to the choice of hyperparameters.</option> <option>C. They may not always learn a meaningful representation of the data.</option> <option>D. All of the above</option> <explanation>Autoencoders have some limitations, such as being computationally expensive to train, sensitive to the choice of hyperparameters, and not always learning a meaningful representation of the data.</explanation>
+
+<question>How can the performance of an autoencoder be evaluated?</question>
+<option>A. By comparing the reconstructed data to the original data.</option> <option>B. By using metrics such as mean squared error (MSE) or peak signal-to-noise ratio (PSNR).</option> <option>C. By evaluating the performance of the autoencoder on downstream tasks.</option> <option>D. All of the above</option> <explanation>The performance of an autoencoder can be evaluated in a number of ways, including by comparing the reconstructed data to the original data, using metrics such as MSE or PSNR, and evaluating the performance of the autoencoder on downstream tasks.</explanation>
+
+<question>How can the bottleneck layer in an autoencoder be used for feature extraction?</question>
+<option>A. By training the autoencoder to reconstruct the input data as accurately as possible.</option> <option>B. By training the autoencoder to learn a compressed representation of the data that captures the most important features.</option> <option>C. By using the output of the bottleneck layer as input to another machine learning model.</option> <option>D. All of the above</option> <explanation>The bottleneck layer in an autoencoder can be used for feature extraction by training the autoencoder to learn a compressed representation of the data that captures the most important features. The output of the bottleneck layer can then be used as input to another machine learning model.</explanation>
+
+<question>How can autoencoders be used for anomaly detection?</question>
+<option>A. By training the autoencoder to reconstruct normal data as accurately as possible.</option> <option>B. By identifying data points that the autoencoder cannot reconstruct well as anomalies.</option> <option>C. By using the output of the bottleneck layer to train a separate anomaly detection model.</option> <option>D. All of the above</option> <explanation>Autoencoders can be used for anomaly detection by training the autoencoder to reconstruct normal data as accurately as possible. Data points that the autoencoder cannot reconstruct well are then identified as anomalies. Additionally, the output of the bottleneck layer can be used to train a separate anomaly detection model.</explanation>
+
+<question>What are some future research directions for autoencoders?</question>
+<option>A. Developing more efficient and scalable autoencoder architectures.</option> <option>B. Exploring new applications for autoencoders in different domains.</option> <option>C. Investigating the theoretical properties of autoencoders and their ability to learn meaningful representations.</option> <option>D. All of the above</option> <explanation>There are many exciting future research directions for autoencoders, including developing more efficient and scalable autoencoder architectures, exploring new applications for autoencoders in different domains, and investigating the theoretical properties of autoencoders and their ability to learn meaningful representations.</explanation>
+
+<answers>A,B,C,D,D,D,D,D,D,D</answers>
+  `;
 function GenMaterial() {
   const nagivate = useNavigate();
+  const [showQuiz, setShowQuiz] = useState(false);
   const [generatedQuiz, setGeneratedQuiz] = useState(null);
 
   const WarningMessage = `**Warning: Potential Harmful Content Detected**
@@ -28,7 +62,8 @@ function GenMaterial() {
   };
   const generateQuiz = () => {
     const quiz = location.state?.quiz;
-    setGeneratedQuiz(quiz)
+    setGeneratedQuiz(DemoQuizContent);
+    setShowQuiz(true);
   }
 
     const location = useLocation();
@@ -44,12 +79,7 @@ function GenMaterial() {
         }
     </Box>
     <Button onClick={generateQuiz} size='lg'>Quiz</Button>
-      {
-        generatedQuiz
-            ? (
-                <Box textAlign={"left"}><Markdown>{generatedQuiz}</Markdown></Box>)
-            : <Text> Click on the Quiz Button to See the Quiz</Text>
-      }
+      {showQuiz && <MCQQuestion quizContent={generatedQuiz} />}
     <Button onClick={navigateToHome} size='lg'>Back to Home Page</Button>
     </Stack>
   );
--- src/pages/MCQQuestion.jsx
+++ src/pages/MCQQuestion.jsx
@@ -1,41 +1,110 @@
 import { useState } from 'react';
-import { Box, Heading, Radio, RadioGroup, Stack, Text } from '@chakra-ui/react';
+import {Box, Button, Flex, Heading,  Spacer, Text, VStack} from '@chakra-ui/react';
 
-const MCQQuestion = ({ question, options, correctAnswer }) => {
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [showAnswer, setShowAnswer] = useState(false);
+const extractQuestionsAndAnswers = (quizContent) => {
+    // Split the quiz content into individual documents
+    // Initialize the lists for questions and answers
+    var index_tracker = 0;
+    const questions = [];
+    const options = [];
+    const explanations = [];
 
-    const handleOptionClick = (option) => {
-        setSelectedOption(option);
-        setShowAnswer(true);
+    // Iterate through the documents and extract the questions and answers
+    for(let i = 0; i <10; i++){
+        // Extract the question
+        const questionStart = quizContent.indexOf('<question>',index_tracker) + 10;
+        const questionEnd = quizContent.indexOf('</question>',index_tracker);
+        const question = quizContent.substring(questionStart, questionEnd);
+        //quizContent = quizContent.substring(questionEnd + 10);
+        index_tracker = questionEnd + 11 ;
+        questions.push(question);
+
+        // Extract the answer
+        const options_mcq = []
+        for (let j = 0; j < 4; j++){
+            const answerStart = quizContent.indexOf('<option>',index_tracker) + 8;
+            const answerEnd = quizContent.indexOf('</option>',index_tracker);
+            const option = quizContent.substring(answerStart, answerEnd);
+            //quizContent = quizContent.substring(questionEnd);
+            index_tracker = answerEnd + 9 ;
+            options_mcq.push(option)
+        }
+        options.push(options_mcq);
+
+        const explanationStart = quizContent.indexOf('<explanation>',index_tracker) + 13;
+        const explanationEnd = quizContent.indexOf('</explanation>',index_tracker);
+        const explanation = quizContent.substring(explanationStart, explanationEnd);
+        index_tracker = explanationEnd + 14;
+        explanations.push(explanation);
+
+    }
+    const answersStart = quizContent.indexOf('<answers>',index_tracker) + 9;
+    const answersEnd = quizContent.indexOf('</answers>',index_tracker);
+    const answer_str = quizContent.substring(answersStart, answersEnd);
+    const answers = answer_str.split(",");
+
+
+    return { questions, options, explanations, answers };
+};
+const MCQQuestion = ({quizContent}) => {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [isCorrect, setIsCorrect] = useState(null);
+    const [explanation, setExplanation] = useState('');
+
+    const {questions,options,explanations, answers} = extractQuestionsAndAnswers(quizContent);
+
+    const handleNextQuestion = () => {
+        setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+        setSelectedAnswer(null);
+        setIsCorrect(null);
+        setExplanation('');
+    };
+    const handleAnswerSelect = (option) => {
+        setSelectedAnswer(option);
+        const correctAnswer = answers[currentQuestionIndex];
+        setIsCorrect(option[0] === correctAnswer);
+        setExplanation(explanations[currentQuestionIndex]); // Explanation is the second item in the inner list
     };
 
     return (
-        <Box>
-            <Heading size="md" mb={4}>
-                {question}
-            </Heading>
-            <RadioGroup onChange={handleOptionClick} value={selectedOption}>
-                <Stack spacing={3}>
-                    {options.map((option, index) => (
-                        <Radio
-                            key={index}
-                            value={option}
-                            colorScheme={showAnswer && option === correctAnswer ? 'green' : 'gray'}
-                            fontWeight={showAnswer && option === selectedOption ? 'bold' : 'normal'}
-                        >
-                            {option}
-                        </Radio>
-                    ))}
-                </Stack>
-            </RadioGroup>
-            {showAnswer && (
-                <Text mt={4} color="green.500">
-                    Correct Answer: {correctAnswer}
-                </Text>
-            )}
+        <Box p={10} borderWidth={1} borderRadius={8} boxShadow="lg">
+            <VStack spacing={6} align="start">
+                <Heading size="xl">Quiz</Heading>
+                <Box>
+                    <Heading size="md" mb={4}>{questions[currentQuestionIndex]}</Heading>
+                    <VStack spacing={4} align="start">
+                        {options[currentQuestionIndex].map((option, index) => (
+                            <Button
+                                key={index}
+                                variant={selectedAnswer === option ? (isCorrect ? 'solid' : 'outline') : 'ghost'}
+                                colorScheme={selectedAnswer === option ? (isCorrect ? 'green' : 'red') : 'gray'}
+                                onClick={() => handleAnswerSelect(option)}
+                            >
+                                 {option}
+                            </Button>
+                        ))}
+                    </VStack>
+                </Box>
+                {isCorrect !== null && (
+                    <Box>
+                        <Flex align="center">
+                            <Text fontSize="lg" fontWeight="bold" color={isCorrect ? 'green.500' : 'red.500'}>
+                                {isCorrect ? 'Correct!' : 'Incorrect.'}
+                            </Text>
+                            <Spacer/>
+                            <Text fontSize="md">{explanation}</Text>
+                        </Flex>
+                    </Box>
+                )}
+                {currentQuestionIndex < questions.length - 1 && (
+                    <Button colorScheme="blue" onClick={handleNextQuestion}>
+                        Next Question
+                    </Button>
+                )}
+            </VStack>
         </Box>
     );
-};
+}
 
 export default MCQQuestion;
